cf36e7bed5425e0af3c4a23c985cc21e2b28a04e
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-04 00:50:22 +0700

    Fetch products from api

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function Home() {
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  async function fetchProducts() {
+    const url = "/api/products";
+    const res = await axios.get(url);
+    console.log(res.data);
+  }
+
   return <>home</>;
 }
 
