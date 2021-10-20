b6560c668b0223b3b3941d68e881e79824d6c2cc
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-20 08:05:41 +0700

    Server render Product details

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,18 @@
-function Product() {
+import axios from "axios";
+
+function Product({ product }) {
+  console.log("product: ", product);
   return <>product</>;
 }
+
+Product.getInitialProps = async function ({ query: { _id } }) {
+  const url = "http://localhost:3000/api/product";
+
+  const payload = { params: { _id } };
+
+  const res = await axios.get(url, payload);
+
+  return { product: res.data };
+};
 
 export default Product;
