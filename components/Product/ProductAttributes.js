9d4bda89b07dce548b0b4fdc13f0157b9a63d441
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-22 00:31:21 +0700

    Delete one product front to back

--- components/Product/ProductAttributes.js
+++ components/Product/ProductAttributes.js
@@ -1,15 +1,50 @@
-import { Button, Header } from "semantic-ui-react";
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { Button, Header, Modal } from "semantic-ui-react";
+import baseUrl from "../../utils/baseUrl";
 
-function ProductAttributes({ description }) {
+function ProductAttributes({ description, _id }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    const url = `${baseUrl}/api/product`;
+
+    const payload = { params: { _id } };
+
+    await axios.delete(url, payload);
+
+    router.push("/");
+  };
+
   return (
     <>
       <Header as="h3">About this product </Header>
       <p>{description}</p>
       <Button
+        onClick={() => setModalOpen(true)}
         icon="trash alternate outline"
         color="red"
         content="Delete Product"
       />
+      <Modal open={modalOpen} dimmer="blurring">
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are you sure you want to delete this product?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button content="Cancel" onClick={() => setModalOpen(false)} />
+          <Button
+            onClick={handleDelete}
+            negative
+            icon="trash"
+            labelPosition="right"
+            content="Delete"
+          />
+        </Modal.Actions>
+      </Modal>
     </>
   );
 }
--- pages/api/product.js
+++ pages/api/product.js
@@ -1,7 +1,32 @@
 import Product from "../../models/Product";
 
 export default async (req, res) => {
-  const product = await Product.findOne({ _id: req.query._id });
+  switch (req.method) {
+    case "GET":
+      await handleGetReq(req, res);
+      break;
+    case "DELETE":
+      await handleDeleteReq(req, res);
+      break;
+    default:
+      res
+        .status(405)
+        .send(`Method ${req.method} is not allowed for this endpoint.`);
+  }
+};
+
+async function handleGetReq(req, res) {
+  const { _id } = req.query;
+
+  const product = await Product.findOne({ _id });
 
   res.status(200).json(product);
-};
+}
+
+async function handleDeleteReq(req, res) {
+  const { _id } = req.query;
+
+  await Product.findByIdAndDelete({ _id });
+
+  res.status(204).json({});
+}
