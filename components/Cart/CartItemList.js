5bd38833f210f2587d3c5260a465748da72351ef
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-23 00:57:25 +0700

    Add Cart UI

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,23 @@
+import { Button, Header, Icon, Segment } from "semantic-ui-react";
+
 function CartItemList() {
-  return <>CartItemList</>;
+  const user = true;
+
+  return (
+    <Segment secondary color="teal" inverted textAlign="center" placeholder>
+      <Header icon>
+        <Icon name="shopping basket" />
+        No product in your cart. Add some?
+      </Header>
+      <div>
+        {user ? (
+          <Button color="orange">View Products</Button>
+        ) : (
+          <Button color="blue">Login to Add Products</Button>
+        )}
+      </div>
+    </Segment>
+  );
 }
 
 export default CartItemList;
