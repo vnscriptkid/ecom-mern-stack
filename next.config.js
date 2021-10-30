373e31c9b7d3d3c4ec2211d3fc81ae05fbae6102
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-30 17:09:36 +0700

    No track next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_SRV: "mongodb://localhost:27017/react-reserve",
-    JWT_SECRET: "sdf;ajlksdf;alksdfjlasdkjfswieriw*#(@#*#&*sdjhfjs)",
-    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/vnscriptkid/image/upload",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>",
-  },
-};
